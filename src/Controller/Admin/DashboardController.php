f94cdc80af8c328169dab7a5326f4474b8c95e34
mithridatem <mathieumithridate@adrar-formation.com>
2024-02-29 22:04:09 +0100

    feat: panel-admin flag

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Flag;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator){}
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator
+        ->setController(FlagCrudController::class)
+        ->generateUrl();
+        return $this->redirect($url);
+    }
+
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Ctf Panel');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToRoute('Retour à l\'accueil', 'fa fa-home', 'app_register');
+        yield MenuItem::linkToCrud('Flag', 'fa-solid fa-flag', Flag::class);
+
+    }
+}
