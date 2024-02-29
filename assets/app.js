967bf40b0c9e5e0ff37d14b667a091f7b5ebeca3
mithridatem <mathieumithridate@adrar-formation.com>
2024-02-29 19:58:26 +0100

    feat: config tailwindcss

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
+import './styles/app.tailwind.css';
+import './scripts/preline.js';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
