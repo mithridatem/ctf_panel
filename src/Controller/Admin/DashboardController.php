348e0b439b8306a4fc10b834f47429e62a2a212e
mithridatem <mathieumithridate@adrar-formation.com>
2024-02-29 23:21:24 +0100

    feat: authentification admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends AbstractDashboardController
 {
     public function __construct(private AdminUrlGenerator $adminUrlGenerator){}
 
-    #[Route('/admin', name: 'admin')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
         $url = $this->adminUrlGenerator
