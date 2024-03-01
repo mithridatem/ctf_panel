99c2a64e59d87bfa0fecccaeb5f13dc1efc08c40
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-01 10:19:59 +0100

    fix : design validation des flags

--- src/Form/ScoreType.php
+++ src/Form/ScoreType.php
@@ -19,11 +19,22 @@ class ScoreType extends AbstractType
             ->add('participant', EntityType::class, [
                 'class' => Participant::class,
                 'choice_label' => 'login',
+                'attr' => ['class' => 'py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600',],
+                'label' => 'Choisir votre login dans la liste',
+                'label_attr' => [
+                    'class' => 'block text-sm mb-2 dark:text-white',
+                ]
             ])
-            ->add('Flag', TextType::class,[
+            ->add('Flag', TextType::class, [
+                'attr' => [
+                    'class' => 'py-3 px-4 block w-full border-gray-600 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600',
+                ],
                 'mapped' => false,
-            ])
-            ->add('Envoyer', SubmitType::class);
+                'label' => 'Entrer le code',
+                'label_attr' => [
+                    'class' => 'block text-sm mb-2 dark:text-white'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
