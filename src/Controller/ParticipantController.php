84021b58635972b7002aa41027f74474a7a97eae
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-10 12:03:55 +0100

    api inscription

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Service\UtilsService;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class ParticipantController extends AbstractController
@@ -18,11 +19,15 @@ class ParticipantController extends AbstractController
     private ParticipantRepository $participantRepository;
     private EntityManagerInterface $em;
 
-    public function __construct(ParticipantRepository $participantRepository, EntityManagerInterface $em) 
+    private SerializerInterface $serializer;
+
+    public function __construct(ParticipantRepository $participantRepository, EntityManagerInterface $em, SerializerInterface $serializer) 
     {
         $this->participantRepository = $participantRepository;
 
         $this->em = $em;
+
+        $this->serializer = $serializer;
     }
 
     #[Route('/participant/add', name: 'app_participant_add')]
@@ -53,5 +58,38 @@ class ParticipantController extends AbstractController
         return $this->render('participant/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/api/participant/add', name: 'app_participant_add_api', methods:'POST')]
+    public function addParticipantApi(Request $request) : Response 
+    {
+        $msg = [];
+        $code = 200;
+        $json = $request->getContent();
+        //test si le json est valide
+        if($request){
+            //conversion
+            $data = $this->serializer->decode($json, 'json');
+            //vérification des données
+            if($data["nom"]!="" and $data["prenom"]!="") {
+                //récup du compte
+                $part = $this->participantRepository->findOneBy(["nom"=>$data["nom"], "prenom"=>$data["prenom"]]);
+                if($part) {
+                    $msg = "Vous étes déja inscrit avec le login : " . $part->getLogin();
+                }
+                else {
+                    
+                    $participant = new Participant();
+                    $participant->setNom(UtilsService::cleanInput($data["nom"]));
+                    $participant->setPrenom(UtilsService::cleanInput($data["prenom"]));
+                    $participant->setLogin(substr($participant->getPrenom(),0,1) . $participant->getNom());
+                    $participant->setActive(true);
+                    $this->em->persist($participant);
+                    $this->em->flush();
+                    $msg = "Inscription réussi, votre login est : " . $participant->getLogin();
+                }
+            }
+        }
+        return $this->json(["message"=>$msg],$code, ['Content-Type' => 'application/json', 'Access-Control-Allow-Origin' => '*']);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
+    "./src/Form/*.html.twig",
   ],
   theme: {
     extend: {},
